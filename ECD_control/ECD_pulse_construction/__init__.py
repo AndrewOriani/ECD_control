1b968c8b729e7e2606e7c7cbfbf762b5439ee5b7
AndrewOriani <32420759+AndrewOriani@users.noreply.github.com>
2022-02-14 14:26:29 -0600

    Added conditional functions to __init__ import

--- ECD_control/ECD_pulse_construction/__init__.py
+++ ECD_control/ECD_pulse_construction/__init__.py
@@ -1,3 +1,3 @@
-from .ECD_pulse_construction import FakePulse, FakeQubit, FakeStorage
+from .ECD_pulse_construction import FakePulse, FakeQubit, FakeStorage, conditional_displacement, conditional_displacement_circuit
 
-__all__=['ECD_pulse_construction', 'FakePulse', 'FakeQubit', 'FakeStorage']
+__all__=['ECD_pulse_construction', 'FakePulse', 'FakeQubit', 'FakeStorage', 'conditional_displacement', 'conditional_displacement_circuit']
